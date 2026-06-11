1fac5773347f6479245b04c449d32267649bd6d7
Robin Chappalley <robin@chapi.ch>
2026-06-11 16:20:18 +0200

    Generate URLs in a file

--- benchmark/test_url.php
+++ benchmark/test_url.php
@@ -1,8 +1,10 @@
 <?php
 
+$outputFile = $argv[1] ?? __DIR__ . '/images-urls';
+
 $numberOfJpg = 8;
 $numberOfpng = 4;
-
+$numberOfImages = $numberOfJpg + $numberOfpng;
 
 $keyHex = "7365637265746b657931323334353637";
 $saltHex = "73656372657473616c74313233343536";
@@ -10,22 +12,31 @@ $saltHex = "73656372657473616c74313233343536";
 $key = pack("H*", $keyHex);
 $salt = pack("H*", $saltHex);
 
+$urls = [];
 
-for ($i = 1; $i <= $numberOfJpg; $i++) {
+for ($i = 1; $i <= $numberOfImages; $i++) {
+    if ($i <= $numberOfJpg) {
+        $format = "jpg";
+    } else {
+        $format = "png";
+    }
 
-// L'image d'origine (Wikipedia)
-$imageUrl = "https://image.chapi.ch/wp-content/uploads/raw/image-test-$i.jpg";
+    $timestamp = time();
+    // L'image d'origine (Wikipedia)
+    $imageUrl = "https://image.chapi.ch/wp-content/uploads/raw/v-$timestamp/image-test-$i.$format";
 
-// On encode l'URL de l'image
-$encodedUrl = rtrim(strtr(base64_encode($imageUrl), '+/', '-_'), '=');
+    // On encode l'URL de l'image
+    $encodedUrl = rtrim(strtr(base64_encode($imageUrl), '+/', '-_'), '=');
 
-// On définit les paramètres de transformation (redimensionnement à 400x400)
-$path = "/format:auto/" . $encodedUrl;
+    // On définit les paramètres de transformation (redimensionnement à 400x400)
+    $path = "/" . $encodedUrl;
 
-// On calcule la signature
-$signature = rtrim(strtr(base64_encode(hash_hmac('sha256', $salt . $path, $key, true)), '+/', '-_'), '=');
+    // On calcule la signature
+    $signature = rtrim(strtr(base64_encode(hash_hmac('sha256', $salt . $path, $key, true)), '+/', '-_'), '=');
 
-// On assemble l'URL finale avec ton IP
-echo "Ton URL signée est : \n";
-echo "https://resize.chapi.ch/" . $signature . $path . "\n";
+    $urls[] = "https://resize.chapi.ch/" . $signature . $path;
 }
+
+file_put_contents($outputFile, implode(PHP_EOL, $urls) . PHP_EOL);
+
+echo "URLs écrites dans : {$outputFile}\n";
