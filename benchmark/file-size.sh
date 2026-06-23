4541ded9c69858c83107bd55688f4b9edff35ef3
Robin Chappalley <robin@chapi.ch>
2026-06-23 16:11:00 +0200

    update script and results about file size

--- benchmark/file-size.sh
+++ benchmark/file-size.sh
@@ -7,16 +7,21 @@ DOSSIER=$(pwd)
 FICHIER_CSV="$DOSSIER/taille_fichiers.csv"
 
 # On écrit l'en-tête du fichier CSV
-echo "Nom du fichier;Poids (Bytes)" > "$FICHIER_CSV"
+echo "Nom du fichier;Extension;Poids (Bytes)" > "$FICHIER_CSV"
 
 # On boucle sur les fichiers du dossier
 for fichier in *; do
   # On vérifie si c'est un fichier (et pas un dossier)
   if [ -f "$fichier" ]; then
+    # On récupère l'extension du fichier
+    EXTENSION="${fichier##*.}"
+    if [ "$fichier" = "$EXTENSION" ]; then
+      EXTENSION=""
+    fi
     # On récupère la taille du fichier en Bytes
     TAILLE=$(stat -c%s "$fichier")
-    # On écrit le nom et la taille du fichier dans le CSV
-    echo "$fichier,$TAILLE" >> "$FICHIER_CSV"
+    # On écrit le nom, l'extension et la taille du fichier dans le CSV
+    echo "$fichier;$EXTENSION;$TAILLE" >> "$FICHIER_CSV"
   fi
 done
 
